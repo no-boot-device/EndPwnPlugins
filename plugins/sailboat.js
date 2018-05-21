ebd0cbf4b0b7630ee224e8d622bcc09693dc6ce4
Flex <lumodder@gmail.com>
2018-05-21 16:57:08 -0600

    fix sailboat so pluginsrepo can parse it correctly

--- plugins/sailboat.js
+++ plugins/sailboat.js
@@ -1,13 +1,15 @@
-exports.manifest = {
-    author: "Adryd, BlockBuilder57, Cynosphere",
-    name: "Sailboat",
-    description: "Login as bot accounts.",
-	replacements = [
-		{signature:'/i.default.Store.pauseEmittingChanges\\(\\),t.user.bot/',payload:'false'},
-		{signature:'/.\\._reset\\(!0,1e3,"Disconnect requested by user"\\)/',payload:'console.log("delightfully devilish seymour")'},
-		{signature:'/\\.experiments;v={},t\\.forEach/',payload:'.experiments;if(t==undefined) return;v={},t.forEach'},
-		{signature:'/CONNECTION_OPEN:function\\(e\\){f=e\\.connected/',payload:'CONNECTION_OPEN:function(e){if(e.connectedAccounts==undefined) return; f=e.connected'},
-		{signature:'/function S\\(e\\){var t=arguments/',payload:'function S(e){if(e==undefined) return; var t=arguments'},
-		{signature:'totalUnavailableGuilds,n=[];',payload:'totalUnavailableGuilds,n=[];if(e.readState==undefined) return;'}
-	]
+exports = {
+	manifest:{
+		author: "Adryd, BlockBuilder57, Cynosphere",
+		name: "Sailboat",
+		description: "Login as bot accounts.",
+		replacements = [
+			{signature:'/i.default.Store.pauseEmittingChanges\\(\\),t.user.bot/',payload:'false'},
+			{signature:'/.\\._reset\\(!0,1e3,"Disconnect requested by user"\\)/',payload:'console.log("delightfully devilish seymour")'},
+			{signature:'/\\.experiments;v={},t\\.forEach/',payload:'.experiments;if(t==undefined) return;v={},t.forEach'},
+			{signature:'/CONNECTION_OPEN:function\\(e\\){f=e\\.connected/',payload:'CONNECTION_OPEN:function(e){if(e.connectedAccounts==undefined) return; f=e.connected'},
+			{signature:'/function S\\(e\\){var t=arguments/',payload:'function S(e){if(e==undefined) return; var t=arguments'},
+			{signature:'totalUnavailableGuilds,n=[];',payload:'totalUnavailableGuilds,n=[];if(e.readState==undefined) return;'}
+		]
+	}
 }
